47076b5792a0dec29b45d246a40dec67d4b65a2b
Bl00g000 <49353322+Bl00g000@users.noreply.github.com>
2024-10-07 12:33:39 +1300

    Error Fix for IntThreads

--- Assets/Scripts/Abilities/Fred Weeva/Thread.cs
+++ Assets/Scripts/Abilities/Fred Weeva/Thread.cs
@@ -3,40 +3,6 @@ using VInspector;
 
 public class Thread : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    bool isThreadActive = false;
-
-    [Tab("Data")]
-    public Material activeMaterial;
-    public Material inactiveMaterial;
-
-    [Tab("References")]
-    MeshRenderer threadMeshRenderer;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void ChangeThreadState(bool _isActive)
-    {
-        isThreadActive = _isActive;
-
-        if (isThreadActive)
-        {
-            threadMeshRenderer.materials[0] = activeMaterial;
-        }
-        else
-        {
-            threadMeshRenderer.materials[0] = inactiveMaterial;
-=======
     public GameObject mesh;
     MeshRenderer meshRenderer;
     public Material activeMat;
@@ -62,7 +28,6 @@ public class Thread : MonoBehaviour
         else
         {
             meshRenderer.material = inactiveMat;
->>>>>>> Stashed changes
         }
     }
 }
