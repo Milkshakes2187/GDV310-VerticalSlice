94840fa710586c66126ce50c03dd43760d08b91f
George White <tgeorgew465@gmail.com>
2024-10-06 15:24:36 +1300

    ugh my brain is in the slow mobile

--- Assets/Scripts/Abilities/Ability.cs
+++ Assets/Scripts/Abilities/Ability.cs
@@ -1,10 +1,32 @@
 using UnityEngine;
 
+
+public enum E_CastStyle
+{
+    INSTANT,
+    CAST_TIME,
+    CHANNEL,
+    TOGGLE,
+}
+
 public class Ability : MonoBehaviour
 {
-    float castTime = 0;
+    
+
+    bool bCastWhileMoving = false;
+
+    float fCastTime = 0.0f;
+    float fCooldown = 0.0f;
+
+    E_CastStyle castStyle = E_CastStyle.CAST_TIME;
+
+    float fStrength = 0.0f;
 
     // cooldown
     // damage
     // etc
+
+
+
+
 }
