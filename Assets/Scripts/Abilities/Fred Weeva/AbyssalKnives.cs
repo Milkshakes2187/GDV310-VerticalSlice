8230356bf457cf0ea47fca2cf39b435c87fb7444
Bloo <49353322+Bl00g000@users.noreply.github.com>
2024-10-13 14:55:47 +1300

    Abyssal Knievs implemented

---
+++ Assets/Scripts/Abilities/Fred Weeva/AbyssalKnives.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbyssalKnives : MonoBehaviour
+{
+    // TODO: make sure to rework this ability to work with georges ability system
+
+    public Vector3 targetDirection;
+    public GameObject projectilePF;
+
+    List<GameObject> projectiles = new List<GameObject>();
+
+    public float projectileSpeed = 5f;
+    public float range = 10f;
+
+
+    private void Start()
+    {
+        targetDirection.y = transform.position.y;
+
+        ActivateAbility();
+    }
+
+    private void Update()
+    {
+        if (projectiles.Count > 0)
+        {
+            foreach (var projectile in projectiles)
+            {
+                if (projectile)
+                {
+                    projectile.transform.position += projectile.transform.forward * projectileSpeed * Time.deltaTime;
+                }
+            }
+        }
+    }
+
+    void ActivateAbility()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            var projectile = Instantiate(projectilePF, transform.position, Quaternion.identity);
+            Destroy(projectile, range / projectileSpeed);
+
+            projectiles.Add(projectile);
+        }
+
+        float angleOffset = 10;
+        
+
+        projectiles[0].transform.LookAt(targetDirection);
+
+        Quaternion leftRotation = Quaternion.Euler(0, -angleOffset, 0);
+        projectiles[1].transform.rotation = Quaternion.LookRotation(leftRotation * (targetDirection - projectiles[1].transform.position));
+
+        Quaternion rightRotation = Quaternion.Euler(0, angleOffset, 0);
+        projectiles[2].transform.rotation = Quaternion.LookRotation(rightRotation * (targetDirection - projectiles[2].transform.position));
+    }
+}
