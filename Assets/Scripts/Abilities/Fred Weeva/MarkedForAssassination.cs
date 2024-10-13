ed271d895e317e2b1ccc9a562358d7282fcdc6ea
Bloo <49353322+Bl00g000@users.noreply.github.com>
2024-10-13 15:13:22 +1300

    Marked for assassination implemented
    (echos don't do anything yet)

---
+++ Assets/Scripts/Abilities/Fred Weeva/MarkedForAssassination.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class MarkedForAssassination : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject indicatorPF;
+    public GameObject echoPF;
+
+    GameObject indicator;
+
+    public float castTime = 5f;
+    public float elapsedTime = 0f;
+
+    private void Start()
+    {
+        indicator = Instantiate(indicatorPF, target.transform.position, Quaternion.identity);
+        Destroy(indicator, castTime);
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > castTime)
+        {
+            ActivateAbility();
+            Destroy(gameObject);
+        }
+
+        if (indicator)
+        {
+            indicator.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 4, target.transform.position.z);
+        }
+    }
+
+    void ActivateAbility()
+    {
+        Instantiate(echoPF, target.transform.position, Quaternion.identity);
+    }
+}
