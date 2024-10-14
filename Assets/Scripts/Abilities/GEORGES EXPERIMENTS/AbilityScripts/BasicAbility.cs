1aa812f9d3d5f5e35f1ab29bbe7fc731378bc741
George White <tgeorgew465@gmail.com>
2024-10-14 13:59:54 +1300

    Additions to the abilitySO, commenting and clening

--- Assets/Scripts/Abilities/GEORGES EXPERIMENTS/AbilityScripts/BasicAbility.cs
+++ Assets/Scripts/Abilities/GEORGES EXPERIMENTS/AbilityScripts/BasicAbility.cs
@@ -2,26 +2,27 @@ using UnityEngine;
 
 public class BasicAbility : Ability
 {
-
-    [SerializeField] float damage = 0.0f;
+    //Gameobject that gets instantiated upon ability use
     [SerializeField] GameObject tempAttack = null;
 
-
-    //Temporary - use the players basic ability
+    /***********************************************
+   * UseSpellEffect: overridden function. Creates a sphere above the spellsystem temporarily
+   * @author: George White
+   * @parameter:
+   * @return: override void
+   ************************************************/
     public override void UseSpellEffect()
     {
-        
-          //temporary - while we have no animations. spawns a a cube above the player
-          if(tempAttack)
-          {
-              var tempobj =  Instantiate(tempAttack, new Vector3(targetLocation.x, targetLocation.y + damage, targetLocation.z), Quaternion.identity);
-              Destroy(tempobj, 1);
-          }
-
+         //temporary - while we have no animations. spawns a a cube above the player
+         if(tempAttack)
+         {
+             var tempobj =  Instantiate(tempAttack, new Vector3(targetLocation.x, targetLocation.y + abilityData.primaryDamage, targetLocation.z), Quaternion.identity);
+             Destroy(tempobj, 1);
+         }
 
         //animation trigger, maybe do sword
 
-
+        //Destroy the ability
         Destroy(gameObject);
     }
 
