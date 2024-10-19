0e8fe847332dbd15d8c3be8e2e4b8a2c0254eacb
Nathan Hunt <nathanpmhunt@yahoo.co.nz>
2024-10-19 15:42:19 +1300

    ui updates

--- Assets/Scripts/UserInterface/Controls/MPUIGradientController.cs
+++ Assets/Scripts/UserInterface/Controls/MPUIGradientController.cs
@@ -8,26 +8,40 @@ using VInspector;
 [ExecuteInEditMode]
 public class MPUIGradientController : MonoBehaviour
 {
+    public bool baseColorControls = false;
+
+    [ShowIf("baseColorControls")] 
+        public Color baseColor;
+    [EndIf]
+
     public List<MPUIKIT.MPImage> gradientImages;
     public GradientEffect gradient;
 
     // Update is called once per frame
     //
 
-    [OnValueChanged("gradient")]
+    [OnValueChanged("gradient", "baseColorControls", "baseColor", "gradientImages")]
     void UpdateGradient()
     {
         if (gradientImages == null) return;
 
         foreach (MPImage _mpImg in gradientImages)
         {
-            _mpImg.GradientEffect = new GradientEffect
+            if (gradient.Enabled)
             {
-                Enabled = true,
-                Gradient = gradient.Gradient,
-                GradientType = gradient.GradientType,
-                Rotation = gradient.Rotation
-            };
+                _mpImg.GradientEffect = new GradientEffect
+                {
+                    Enabled = true,
+                    Gradient = gradient.Gradient,
+                    GradientType = gradient.GradientType,
+                    Rotation = gradient.Rotation
+                };
+            }
+
+            if(baseColorControls)
+            {
+                _mpImg.color = baseColor;
+            }
         }
     }
 }
