11030f9a5564da5853ccfe27114f762d4e5b5550
Nathan Hunt <nathanpmhunt@yahoo.co.nz>
2024-10-21 14:20:04 +1300

    ability holder ui controller

---
+++ Assets/Scripts/UserInterface/Controls/UIAbilityHolderController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class UIAbilityHolderController : MonoBehaviour
+{
+
+    [Header("Preassigned Variables")]
+    public UIFillController cooldownFill;
+    public UIFillController lockFill;
+
+    public ability
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
